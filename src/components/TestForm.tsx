87bba5f2cf75406d2a677857397dad7879f57e79
Luke Thompson <luke.thompson@nine.com.au>
2024-08-26 20:29:59 +1000

    changed netlify attribute name

--- src/components/TestForm.tsx
+++ src/components/TestForm.tsx
@@ -1,28 +1,6 @@
 const TestForm = () => {
-    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
-
-        // Cast event.currentTarget to HTMLFormElement
-        const myForm = event.currentTarget as HTMLFormElement;
-        const formData = new FormData(myForm);
-
-        // Convert FormData to URLSearchParams
-        const urlSearchParams = new URLSearchParams();
-        formData.forEach((value, key) => {
-            urlSearchParams.append(key, value as string); // Ensure value is a string
-        });
-
-        fetch("/", {
-            method: "POST",
-            headers: { "Content-Type": "application/x-www-form-urlencoded" },
-            body: urlSearchParams.toString(),
-        })
-        .then(() => console.log("Form successfully submitted"))
-        .catch((error) => console.error("Error submitting form:", error));
-    };
-
     return (
-        <form name="contact" method="POST" onSubmit={handleSubmit}>
+        <form name="contact" method="POST" data-netlify="true">
             <input type="hidden" name="form-name" value="contact" />
             <p>
                 <label>Your Name: <input type="text" name="name" /></label>
