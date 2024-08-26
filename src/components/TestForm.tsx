a3079badf30674bf87a0a4d5fb58048324fa750b
Luke Thompson <luke.thompson@nine.com.au>
2024-08-26 19:46:33 +1000

    changed netlify attribute name

--- src/components/TestForm.tsx
+++ src/components/TestForm.tsx
@@ -10,7 +10,7 @@ const TestForm = () => {
                 <label>Your Email: <input type="email" name="email" /></label>
             </p>
             <p>
-                <label>Postcode: <input type="postcode" name="postcode" /></label>
+                <label>Postcode: <input type="text" name="postcode" /></label>
             </p>
             <p>
                 <button type="submit">Send</button>
