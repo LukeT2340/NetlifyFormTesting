2b55c80d6f0f4bc0e8509e51b0212e7334b213c4
Luke Thompson <luke.thompson@nine.com.au>
2024-08-26 16:27:54 +1000

    Created test form for netlify

---
+++ src/components/TestForm.tsx
@@ -0,0 +1,21 @@
+
+const TestForm = () => {
+    return (
+        <form name="contact" method="POST" data-netlify="true">
+            <p>
+                <label>Your Name: <input type="text" name="name" /></label>
+            </p>
+            <p>
+                <label>Your Email: <input type="email" name="email" /></label>
+            </p>
+            <p>
+                <label>Postcode: <input type="postcode" name="postcode" /></label>
+            </p>
+            <p>
+                <button type="submit">Send</button>
+            </p>
+        </form>
+    )
+}
+
+export default TestForm
