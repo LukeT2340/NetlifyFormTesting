8c1348939d6ca91ff35e6b923184686ebac276ab
Luke Thompson <luke.thompson@nine.com.au>
2024-08-26 20:32:31 +1000

    changed netlify attribute name

--- src/components/TestForm.tsx
+++ src/components/TestForm.tsx
@@ -1,15 +1,9 @@
 const TestForm = () => {
     return (
-        <form name="contact" method="POST" data-netlify="true">
-            <input type="hidden" name="form-name" value="contact" />
+        <form name="test" method="POST" data-netlify="true">
+            <input type="hidden" name="form-name" value="test" />
             <p>
                 <label>Your Name: <input type="text" name="name" /></label>
-            </p>
-            <p>
-                <label>Your Email: <input type="email" name="email" /></label>
-            </p>
-            <p>
-                <label>Postcode: <input type="text" name="postcode" /></label>
             </p>
             <p>
                 <button type="submit">Send</button>
