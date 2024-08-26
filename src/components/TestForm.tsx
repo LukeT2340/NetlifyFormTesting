c413b1881721e1397ad1ff9c58f17cb1f4f94a94
Luke Thompson <luke.thompson@nine.com.au>
2024-08-26 21:05:35 +1000

    added dummy form to index.html so that netlify knows to listen for post requests

--- src/components/TestForm.tsx
+++ src/components/TestForm.tsx
@@ -6,6 +6,12 @@ const TestForm = () => {
                 <label>Name: <input type="text" name="name" required /></label>
             </p>
             <p>
+                <label>Email: <input type="email" name="email" required /></label>
+            </p>
+            <p>
+                <label>Postcode: <input type="text" pattern="[0-9]{4}" name="postcode" required /></label>
+            </p>
+            <p>
                 <button type="submit">Submit</button>
             </p>
         </form>
